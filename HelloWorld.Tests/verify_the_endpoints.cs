7d8fc5c043c36d6c9b66b3b84da91200f36518fc
raj <raj.julha@gmail.com>
2019-01-02 19:42:51 +0400

    Initial commit
    Signed-off-by: raj <raj.julha@gmail.com>

---
+++ HelloWorld.Tests/verify_the_endpoints.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Alba;
+namespace HelloWorld.Tests
+{
+    public class verify_the_endpoint
+    {
+        [Fact]
+        public async Task check_it_out()
+        {
+            using (var system = SystemUnderTest.ForStartup<Startup>())
+            {
+                await system.Scenario(s =>
+                {
+                    s.Get.Url("/");
+                    s.ContentShouldBe("Hey, world.");
+                    s.ContentTypeShouldBe("text/plain; charset=utf-8");
+                });
+            }
+        }
+
+    }
+}
